71720a231daf831975a7e7892b137e62d567d0f0
William Schmidt <9williamschmidt@gmail.com>
2021-07-30 20:19:09 -0300

    piramide

--- fundamentos/bloco-4-javascript-primeiros-passos/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-4-javascript-primeiros-passos/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -2,11 +2,24 @@ const n = 5;
 
 let asterisco = "";
 
-for(linha = 0; linha < n; linha += 1){
-    asterisco += "*";
-    for(coluna = 0; coluna < n; coluna += 1){
-        asterisco += "\n";
+if(n > 1){
+    for(height = 0; height < n; height += 1){
+        for(line = 0; line <= height; line += 1){
+            asterisco += "*";
+        }
+        asterisco += "\n"
     }
+
+    // for(line = 0; line < n; line += 1){
+    //     for(i = 0; i < n; i += 1){
+    //         asterisco += "*";
+    //     }
+    //     asterisco += "\n"; 
+    // }
+
+    
 }
+
+
 
 console.log(asterisco);
