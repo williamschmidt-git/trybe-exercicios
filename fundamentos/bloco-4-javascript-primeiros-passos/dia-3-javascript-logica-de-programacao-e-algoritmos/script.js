12dce509a1b600c0745339aae01c86afafcdf6f1
William Schmidt <9williamschmidt@gmail.com>
2021-07-31 18:51:00 -0300

    att

--- fundamentos/bloco-4-javascript-primeiros-passos/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-4-javascript-primeiros-passos/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -1,27 +1,11 @@
-const n = 5;
-
+let n = 5;
 let asterisco = "";
 
-if(n > 1){
-    for(height = 1; height < n; height += 1){
-        for(spaces = n - 1; spaces >= height; spaces -= 1){
-            asterisco += " ";
-        }
-
-        for(line = 0; line <= height; line += 1){
+if(n > 5){
+    for(altura = 0; altura < n; altura += 1){
+        for(linha = 0; linha < altura; linha += 1){
             asterisco += "*";
         }
-        asterisco = "\n"
+        console.log(asterisco);
     }
-
-    // for(line = 0; line < n; line += 1){
-    //     for(i = 0; i < n; i += 1){
-    //         asterisco += "*";
-    //     }
-    //     asterisco += "\n"; 
-    // }
 }
-
-
-
-console.log(asterisco);
