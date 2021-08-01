4856c5415819629981344f05f82c577b3bdbac4f
William Schmidt <9williamschmidt@gmail.com>
2021-08-01 19:01:55 -0300

    exercicios 4.1

--- fundamentos/bloco-4-javascript-primeiros-passos/dia-1-javascript-primeiros-passos/exercicio4.1/script.js
+++ fundamentos/bloco-4-javascript-primeiros-passos/dia-1-javascript-primeiros-passos/exercicio4.1/script.js
@@ -1,25 +1,2 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sum = 0;
-
-// for(let i = 0; i < numbers.length; i += 1){
-//     console.log(numbers[i]);
-// }
-
-// for (let i = 0; i < numbers.length; i += 1){
-//     sum += numbers[i];
-// }
-
-// console.log(sum);
-
-
-for (let i = 0; i < numbers.length; i += 1){
-    sum += numbers[i];
-}
-
-let  mediaAritmetica = sum / (numbers.length);
-
-if(mediaAritmetica > 20){
-    console.log("Valor maior que 20.")
-}else {
-    console.log("Valor menor, ou igual a 20.");
-}
+let a = 0;
+let b = 0;
