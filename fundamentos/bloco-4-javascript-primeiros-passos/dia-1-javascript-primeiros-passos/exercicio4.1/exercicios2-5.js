856d72bc96a7c11e836e5d971ed9b0ff59d6179e
William Schmidt <9williamschmidt@gmail.com>
2021-08-01 20:08:52 -0300

    exercicios

---
+++ fundamentos/bloco-4-javascript-primeiros-passos/dia-1-javascript-primeiros-passos/exercicio4.1/exercicios2-5.js
@@ -0,0 +1,40 @@
+let numbers = [0, 4, 9];
+let comparisonNumber = 0;
+let maiorNumero = 0;
+const positiveOrNegative = 0;
+
+let angulo1 = 61;
+let angulo2 = 60;
+let angulo3 = 60;
+let triangulo = false;
+
+// if(numero1 > numero2){
+//     maiorNumero = numero1;
+//    // console.log(maiorNumero);
+// }else {
+//     maiorNumero = numero2;
+//     //console.log(numero2);
+// }
+
+// for(i = 0; i < numbers.length; i+=1){
+//     if (maiorNumero < numbers[i]){
+//         maiorNumero = numbers[i];
+//     } 
+// }
+// console.log(maiorNumero);
+
+// if(positiveOrNegative > 0){
+//     console.log("Positivo");
+// }
+// else if (positiveOrNegative < 0){
+//     console.log("Negativo");
+// } else {
+//     console.log("Zero.");
+// }
+
+if (angulo1 + angulo2 + angulo3 === 180){
+    triangulo = true;
+    console.log(triangulo);
+} else{
+    console.log(triangulo);
+}
