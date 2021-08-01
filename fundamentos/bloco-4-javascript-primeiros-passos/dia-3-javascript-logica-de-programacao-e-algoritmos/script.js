6ac1c005873288863f638483bac68b03604a5808
William Schmidt <9williamschmidt@gmail.com>
2021-08-01 11:47:36 -0300

    piramide com espacos

--- fundamentos/bloco-4-javascript-primeiros-passos/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-4-javascript-primeiros-passos/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -1,11 +1,30 @@
 let n = 5;
 let asterisco = "";
 
-if(n > 5){
-    for(altura = 0; altura < n; altura += 1){
-        for(linha = 0; linha < altura; linha += 1){
+if(n > 1){
+    for(row = 0; row < n; row += 1){
+        for(spaces = n - 1; spaces > row; spaces -= 1){
+            asterisco += ".";
+        }
+        for(column = 0; column <= row; column += 1){
             asterisco += "*";
         }
-        console.log(asterisco);
+        asterisco += "\n";
     }
+
+    // for (row = 0; row <= n; row += 1){
+    //     for(esquerda = 0; esquerda <= row; esquerda += 2){
+    //         asterisco += "-";
+    //     }
+        
+    //     for (column = 0; column < row; column += 1){
+    //         for(piramide = 1; piramide < column; piramide += 1){
+    //             asterisco += "*";
+    //         } 
+    //     }
+
+    //     asterisco += "\n";
+    // }
 }
+
+console.log(asterisco);
