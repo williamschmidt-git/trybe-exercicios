52840d05b331e9ab7bf8e9bda329905c9e5baa90
William Schmidt <9williamschmidt@gmail.com>
2021-08-01 19:19:29 -0300

    mexendo com input do usuario

--- fundamentos/bloco-4-javascript-primeiros-passos/dia-1-javascript-primeiros-passos/exercicio4.1/script.js
+++ fundamentos/bloco-4-javascript-primeiros-passos/dia-1-javascript-primeiros-passos/exercicio4.1/script.js
@@ -1,2 +1,43 @@
 let a = 0;
 let b = 0;
+let soma;
+let subtracao;
+let multiplicacao;
+let divisao;
+let resto;
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  readline.question(`Qual o operador`, operadores => {
+    switch(operadores){
+        case "+":
+            soma = a + b;
+            console.log(soma);
+            break;
+    
+        case "-":
+            subtracao = a - b;
+            console.log(subtracao);
+            break;
+    
+        case "*":
+            multiplicacao = a * b;
+            console.log(multiplicacao);
+            break;
+        
+        case "/":
+            divisao = a / b;
+            console.log(divisao);
+            break;
+        default: 
+            "sair";
+    
+    }
+    readline.close()
+  })
+
+
+
