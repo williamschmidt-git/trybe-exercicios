e528c1e0bbd487c37d5580a70a03e69c739a5206
William Schmidt <9williamschmidt@gmail.com>
2021-08-01 12:48:01 -0300

    exercicio piramide

--- fundamentos/bloco-4-javascript-primeiros-passos/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-4-javascript-primeiros-passos/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -6,10 +6,10 @@ if(n > 1){
         for(spaces = n - 1; spaces > row; spaces -= 1){
             asterisco += " ";
         }
-        for(column = 0; column <= row; column += 1){
+        for(column = 0; column <= row  - (n - 3); column += 1){
             asterisco += "*";
         }
-        for(column = 0; column < row; column += 1){
+        for(column = 0; column < row - (n - 3); column += 1){
             asterisco += "*";
         }
         asterisco += "\n";
