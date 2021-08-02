e14bcae1c908dc0d8934ac41c88699dc66fa5833
William Schmidt <9williamschmidt@gmail.com>
2021-08-02 16:14:21 -0300

    exercicios, first commit

---
+++ fundamentos/bloco-4-javascript-primeiros-passos/dia-4-javascript-objetos-e-funcoes/script.js
@@ -0,0 +1,13 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim'
+  };
+
+//   console.log("Bem vinda, " + info.personagem);
+//   console.log(info);
+
+  for (let key in info) {
+      console.log(info[key]);
+  }
