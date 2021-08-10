d2ec6296958323445056d97c442c5d7b7a39577a
William Schmidt <9williamschmidt@gmail.com>
2021-08-10 15:08:35 -0300

    ajusta pasta dia1

---
+++ fundamentos/bloco-5-DOM-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/parafixar/script.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('section')[0].style.backgroundColor = 'aquam';
+document.querySelectorAll('section')[1].style.backgroundColor = '#209092';
+
+    let elemento = document.getElementsByTagName('h3');
+    console.log(document.getElementsByTagName('h3'));
+    for (let i = 0; i < elemento.length; i += 1) {
+        elemento[i].style.backgroundColor = 'color';
+        console.log("entrei no for");
+    }
+
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+    }
