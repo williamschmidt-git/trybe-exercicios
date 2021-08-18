f14e4fe699ee7eed1156a8ebfeeffe6ac7a0ea3b
William Schmidt <9williamschmidt@gmail.com>
2021-08-18 20:21:26 -0300

    exercicio 6.1, parte 1

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,18 @@
+let arraySiglaEstado = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RO', 'RS', 'RR', 'SC', 'SE', 'SP', 'TO'];
+let arrayTextoEstado = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato rosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
+
+
+
+
+
+function createOptions() {
+    const tagEstados = document.querySelector('#tag-estado');
+
+    for (let i = 0; i < arraySiglaEstado.length; i += 1) {
+        let teste = document.createElement('option');
+        teste.innerHTML = arraySiglaEstado[i];
+        tagEstados.appendChild(teste);
+    }
+}
+
+createOptions();
