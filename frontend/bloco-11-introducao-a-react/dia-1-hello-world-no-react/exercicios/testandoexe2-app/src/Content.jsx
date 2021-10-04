708d33181112a61ee5e1116e5d80951e50ee0608
William Schmidt <9williamschmidt@gmail.com>
2021-10-04 20:00:01 -0300

    exercicios incompletos

---
+++ frontend/bloco-11-introducao-a-react/dia-1-hello-world-no-react/exercicios/testandoexe2-app/src/Content.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react"
+
+const conteudos = [
+    {
+        conteudo: 'High Order Functions',
+        bloco: 8,
+        status: 'Aprendido'
+    },
+    {
+        conteudo: 'Composicao de Componentes',
+        bloco: 11,
+        status: 'Aprendendo',
+    },
+    {
+        conteudo: 'Composicao de Estados',
+        bloco: 12,
+        status: 'Aprenderei'
+    },
+    {
+        conteudo: 'Redux',
+        bloco: 16,
+        status: 'Aprenderei'
+    },
+];
+
+
+class Content extends Component {
+    render() {
+        return <ul>
+            <li>{`O conteudo é: ${conteudos.map((e) => e.conteudo)}`}</li>
+        </ul>
+    }
+}
+
+export default Content
+
+
