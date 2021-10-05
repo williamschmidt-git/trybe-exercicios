c00cb5b7a3e929aaee7f994a099777bf6bc01487
William Schmidt <9williamschmidt@gmail.com>
2021-10-05 19:26:26 -0300

    teste

---
+++ frontend/bloco-10B-introducao-a-react/dia-2-componentes-react/my-pokedex/src/Pokemon.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PokemonCard from "./PokemonCard";
+
+function Pokemon(props) {
+    return <div>
+        <p>{props.name}</p>
+    </div>
+}
+
+export default Pokemon
