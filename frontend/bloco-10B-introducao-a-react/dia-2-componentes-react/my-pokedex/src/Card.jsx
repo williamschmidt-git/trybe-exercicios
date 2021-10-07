33b2e393ee8aec31109e07d83d32a9f77d14a1f9
William Schmidt <9williamschmidt@gmail.com>
2021-10-07 14:25:42 -0300

    enviando alteracoes

--- frontend/bloco-10B-introducao-a-react/dia-2-componentes-react/my-pokedex/src/Card.jsx
+++ frontend/bloco-10B-introducao-a-react/dia-2-componentes-react/my-pokedex/src/Card.jsx
@@ -1,44 +1,47 @@
 import React from "react";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
 class Card extends React.Component {
-    constructor(props) {
-        super(props)
-    }
+  render() {
+    const {
+      pokemon: {
+        name,
+        id,
+        image,
+        type,
+        averageWeight: { value, measurementUnit },
+      },
+    } = this.props;
 
-    render() {
-        const { pokemon: { name, id, image, type, averageWeight: { value, measurementUnit } } } = this.props
-
-        return (
-            <div>
-                <div key={`${id}`} className="Pokedex">
-                    <div className={`${name}`}>
-                        <p>{name}</p>
-                        <p>{type}</p>
-                        <p>{`Weigth: ${value}${measurementUnit}`}</p>
-                        <img src={image} alt={name}></img>
-                    </div>
-                </div >
-            </div>
-
-        )
-    }
+    return (
+      <div>
+        <div key={`${id}`} className="Pokedex">
+          <div className={`${name}`}>
+            <p>{name}</p>
+            <p>{type}</p>
+            <p>{`Weigth: ${value}${measurementUnit}`}</p>
+            <img src={image} alt={name}></img>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 Card.propTypes = {
-    pokemon: PropTypes.shape({
-        name: PropTypes.string,
-        type: PropTypes.string,
-        image: PropTypes.string
-    })
-}
+  pokemon: PropTypes.shape({
+    name: PropTypes.string,
+    type: PropTypes.string,
+    image: PropTypes.string,
+  }),
+};
 
 Card.defaultProps = {
-    pokemon: {
-        name: '',
-        type: '',
-        image: ''
-    }
-}
+  pokemon: {
+    name: "",
+    type: "",
+    image: "",
+  },
+};
 
-export default Card
+export default Card;
