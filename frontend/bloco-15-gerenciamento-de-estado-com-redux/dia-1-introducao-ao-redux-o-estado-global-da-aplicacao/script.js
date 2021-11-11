a34fe6c2a5db2fc706f26bbb44d397423f317b12
William Schmidt <9williamschmidt@gmail.com>
2021-11-11 16:04:54 -0300

    exercicios 15.1, parte 1

--- frontend/bloco-15-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/script.js
+++ frontend/bloco-15-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/script.js
@@ -1,5 +1,19 @@
 const button_previous = document.getElementById('previous');
 const button_next = document.getElementById('next');
+const button_random = document.getElementById('random');
+const value = document.getElementById('value');
+const container = document.getElementById('container');
+
+function criarCor() {
+  const oneChar = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+  let cor = '#';
+  const aleatorio = () => Math.floor(Math.random() * oneChar.length);
+  for (let i = 0; i < 6; i += 1) {
+      cor += oneChar[aleatorio()];
+  }
+  console.log(cor);
+  return cor;
+}
 
 const ESTADO_INICIAL = {
     colors: ['white', 'black', 'red', 'green', 'blue', 'yellow'],
@@ -18,13 +32,30 @@ const ESTADO_INICIAL = {
     }
   }
 
+  const RANDOM_COLOR = () =>{
+    return {
+      type: 'RANDOM',
+    }
+  }
+
   const reducer = (state = ESTADO_INICIAL, action) => {
       switch (action.type) {
         case "INCREMENT":
-
-          return state.index + 1;
+          return {
+              ...state,
+              index: state.index + 1,
+          }
         case "DECREMENT":
-          return state.index - 1;
+            return {
+                ...state,
+                index: state.index - 1,
+            }
+        case "RANDOM":
+          return {
+             ...state,
+             colors: [...state.colors, criarCor()]
+          }
+        
         default: return state;
       };
   };
@@ -34,16 +65,39 @@ const ESTADO_INICIAL = {
 //   store.subscribe(() => console.log(store.getState()))
   console.log(store.getState());
 
+  // Faça o subscribe da store, alterando o innerHTML da tag com id value para a cor atual e o style do elemento com id container . Você deverá ser capaz de ver as cores mudando ao fundo, e o nome da cor exibida.
 
-
-  button_next.addEventListener('click', () => {
+  button_next.addEventListener('click', (event) => {
+    event.preventDefault();
     store.dispatch(NEXT_COLOR());
     store.subscribe(() => store.getState())
-    console.log(store.getState())
+    const state = store.getState();
+    const index = store.getState().index;
+
+    container.style.backgroundColor = state.colors[index]
+    value.innerHTML = state.colors[index]
   })
 
-  button_previous.addEventListener('click', () => {
+  button_previous.addEventListener('click', (event) => {
+    event.preventDefault();
+
     store.dispatch(PREVIOUS_COLOR());
-    // store.subscribe(() => store.getState())
-    console.log(store.getState());
+    store.subscribe(() => store.getState())
+    
+    const state = store.getState()
+    const index = store.getState().index;
+    container.style.backgroundColor = state.colors[index]
+    value.innerHTML = state.colors[index]
   })
+
+  button_random.addEventListener('click', (event) => {
+    event.preventDefault();
+    store.dispatch(RANDOM_COLOR());
+    store.subscribe(() => store.getState())
+    const state = store.getState();
+    const index = store.getState().index;
+
+    console.log(store.getState().colors)
+  })
+
+  
