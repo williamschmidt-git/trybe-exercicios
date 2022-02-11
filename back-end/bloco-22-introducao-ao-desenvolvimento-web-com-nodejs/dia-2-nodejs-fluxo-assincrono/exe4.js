c7d1313f4c9c6287a2e3f1d8f0938774f7b6622d
William Schmidt <9williamschmidt@gmail.com>
2022-02-11 13:56:10 -0300

    exercicio 5.4

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exe4.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exe4.js
@@ -25,16 +25,44 @@ const file = 'simpsons.json';
 //     return chosenSimpson
 // }
 
-async function removeSimpson(){
-    const simpsons = await fs.readFile(file, 'utf-8')
-    .then((data) => JSON.parse(data));
-    console.log(simpsons)
+// async function removeSimpson(){
+//     const simpsons = await fs.readFile(file, 'utf-8')
+//     .then((data) => JSON.parse(data));
+//     console.log(simpsons)
 
-    const teste = simpsons.filter((e) => e.id !== '10' && e.id !== '6')
-    console.log(teste)
+//     const teste = simpsons.filter((e) => e.id !== '10' && e.id !== '6')
+//     console.log(teste)
 
-    await fs.writeFile('./simpsons.json', JSON.stringify(teste))
+//     await fs.writeFile('./simpsons.json', JSON.stringify(teste))
     
+// }
+
+// removeSimpson();
+
+// async function simpsonsFamily(){
+//     const simpsons = await fs.readFile(file, 'utf-8')
+//     .then((data) => JSON.parse(data))
+
+// const simpsonsFamily = simpsons.filter(simpson => [1, 2, 3, 4].includes(simpson.id)); <-- outra solução
+//     const newFamily = simpsons.filter((e) => {
+//         return e.id === '1' || e.id === '2' || e.id === '3' || e.id === '4'
+//     })
+//     console.log(newFamily);
+
+//     fs.writeFile('./simpsonFamily.json', JSON.stringify(newFamily))
+// }
+
+// simpsonsFamily()
+
+async function addCharacter(){
+    const simpsons = await fs.readFile('./simpsonFamily.json', 'utf-8')
+        .then((data) => JSON.parse(data));
+
+    simpsons.push({
+        id: '8', name: 'Nelson Muntz'
+    })
+
+    fs.writeFile('./simpsonFamily.json', JSON.stringify(simpsons))
 }
 
-removeSimpson()
+addCharacter()
