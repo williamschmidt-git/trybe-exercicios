8eb8fe6c512bd2f26463f1735a5193e18dab9542
William Schmidt <9williamschmidt@gmail.com>
2022-02-11 12:16:52 -0300

    exercicio 5.3

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exe4.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exe4.js
@@ -3,11 +3,38 @@ const fs = require('fs').promises;
 const file = 'simpsons.json';
 
 //exe4
-fs.readFile(file, 'utf-8')
-    .then((data) => {
-        return JSON.parse(data);
-    }).then((result) => {
-        result.map((e) => console.log(`${e.id} - ${e.name}`))
-    })
+// fs.readFile(file, 'utf-8')
+//     .then((data) => {
+//         return JSON.parse(data);
+//     }).then((result) => {
+//         result.map((e) => console.log(`${e.id} - ${e.name}`))
+//     })
 
 
+// async function getById(id){
+//     const simpsons = await fs.readFile(file, 'utf-8')
+//     .then((data) => JSON.parse(data))
+
+//     const chosenSimpson = simpsons.find((e) => e.id === id)
+//     console.log(chosenSimpson)
+
+//     if(!chosenSimpson){
+//         throw new Error('id não encontrado')
+//     }
+
+//     return chosenSimpson
+// }
+
+async function removeSimpson(){
+    const simpsons = await fs.readFile(file, 'utf-8')
+    .then((data) => JSON.parse(data));
+    console.log(simpsons)
+
+    const teste = simpsons.filter((e) => e.id !== '10' && e.id !== '6')
+    console.log(teste)
+
+    await fs.writeFile('./simpsons.json', JSON.stringify(teste))
+    
+}
+
+removeSimpson()
