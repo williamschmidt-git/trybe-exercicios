f03252e979ff957bb9d2c60aa966ccbe936ebc18
William Schmidt <9williamschmidt@gmail.com>
2022-02-16 17:49:11 -0300

    requisit0 1

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/exercicios/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/exercicios/index.js
@@ -1,11 +1,15 @@
+// index.js
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const userRouter = require('./routers/userRouter');
+const PORT = 3002;
 
 const app = express();
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
-app.post('/user/register', (req, res, next) => {
-    const { username } = req.body;
+app.use('/user', userRouter);
 
-    
-})
+app.listen(PORT, () => console.log('Run server http://localhost:3002'));
