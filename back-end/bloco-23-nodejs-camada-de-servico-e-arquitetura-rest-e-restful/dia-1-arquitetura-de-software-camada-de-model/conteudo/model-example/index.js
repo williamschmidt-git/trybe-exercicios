3f49ee5a7c28ed5858291a46036f76a27a32f942
William Schmidt <9williamschmidt@gmail.com>
2022-02-21 21:33:12 -0300

    conteudo

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/conteudo/model-example/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/conteudo/model-example/index.js
@@ -7,6 +7,16 @@
 
     const app = express();
 
+    app.get('/authors/:id', async (req, res) => {
+        const { id } = req.params;
+
+        const newAuthor = await author.findById(id);
+
+        if (!newAuthor) return res.status(404).json({ message: 'Not found' });
+
+        res.status(200).json(newAuthor);
+    });
+
     app.get('/authors', async (req, res) => {
         const authors = await author.getAll();
 
@@ -18,6 +28,16 @@
 
         res.status(200).json(books)
     })
+
+    app.get('/books/:id', async (req, res) => {
+        const { id } = req.params;
+
+        const newBook = await book.findById(id);
+
+        return res.status(200).json(newBook);
+    })
+
+    
 
     const PORT = process.env.PORT || 3000;
 
--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/conteudo/model-example/models/Author.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/conteudo/model-example/models/Author.js
@@ -25,6 +25,25 @@ const serialize = (authorData) => ({
     lastName: authorData.last_name,
 });
 
+const findById = async (id) => {
+    // Repare que substituímos o id por `?` na query.
+    // Depois, ao executá-la, informamos um array com o id para o método `execute`.
+    // O `mysql2` vai realizar, de forma segura, a substituição do `?` pelo id informado.
+    const query = 'SELECT first_name, middle_name, last_name FROM model_example.authors WHERE id = ?'
+    const [ authorData ] = await connection.execute(query, [id]);
+
+    if (authorData.length === 0) return null;
+
+    // Utilizamos [0] para buscar a primeira linha, que deve ser a única no array de resultados, pois estamos buscando por ID.
+    const { firstName, middleName, lastName } = authorData.map(serialize)[0];
+
+    return getNewAuthor({
+        id,
+        firstName,
+        middleName,
+        lastName,
+    });
+};
 
 const getAll = async() => {
     const [authors] = await connection.execute(
@@ -36,4 +55,5 @@ const getAll = async() => {
 
 module.exports = {
     getAll,
+    findById
 };
