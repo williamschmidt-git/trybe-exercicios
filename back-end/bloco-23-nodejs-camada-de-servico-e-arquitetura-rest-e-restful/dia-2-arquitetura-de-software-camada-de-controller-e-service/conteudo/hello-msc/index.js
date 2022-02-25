0d678662b0dcd959bb592746d4736755bd3f7a58
William Schmidt <9williamschmidt@gmail.com>
2022-02-25 10:45:04 -0300

    conteudo

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/conteudo/hello-msc/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/conteudo/hello-msc/index.js
@@ -2,18 +2,21 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const rescue = require('express-rescue');
 
 const Author = require('./controllers/Authors');
+const Book = require('./controllers/Books');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/authors', Author.getAll);
+app.get('/authors', rescue(Author.getAll));
+app.get('/authors/:id', rescue(Author.findById));
+app.post('/authors', rescue(Author.createAuthor));
 
-app.get('/authors/:id', Author.findById);
-
-app.post('/authors', Author.createAuthor);
+app.get('/books', Book.getAll);
+app.get('/books/:id', Book.findById);
 
 const PORT = process.env.PORT || 3000;
 
--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/conteudo/hello-msc/services/Authors.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/conteudo/hello-msc/services/Authors.js
@@ -1,64 +1,71 @@
-// services/Authors.js
+const Author = require('../models/Author');
 
-const Author = require('../models/Authors');
+const create = async (firstName, middleName, lastName) =>
+  Author.create(firstName, middleName, lastName);
 
-const getNewAuthor = (authorData) => {
-  const { id, firstName, middleName, lastName } = authorData;
-
-  const fullName = [firstName, middleName, lastName]
-    .filter((name) => name)
-    .join(' ');
-
-  return {
-    id,
-    firstName,
-    middleName,
-    lastName,
-    name: fullName,
+  const findById = async (id) => {
+    // Solicitamos que o model realize a busca no banco
+    const author = await Author.findById(id);
+  
+    // Caso nenhuma pessoa autora seja encontrada, retornamos um objeto de erro.
+    if (!author) {
+      return {
+        error: {
+          code: 'notFound',
+          message: `Não foi possível encontrar uma pessoa autora com o id ${id}`,
+        },
+      };
+    }
+  
+    // Caso haja uma pessoa autora com o ID informado, retornamos essa pessoa
+    return author;
   };
-};
-
-const isValid = (firstName, middleName, lastName) => {
-  if (!firstName || typeof firstName !== 'string') return false;
-  if (!lastName || typeof lastName !== 'string') return false;
-  if (middleName && typeof middleName !== 'string') return false;
-
-  return true;
-  }
-
-const getAll = async () => {
-  const authors = await Author.getAll();
-
-  return authors.map(getNewAuthor);
-}
-
-const findById = async (id) => {
-  const author = await Author.findById(id);
-
-  if (!author) return null;
-
-  return getNewAuthor(author);
-};
 
 const createAuthor = async (firstName, middleName, lastName) => {
-    const validAuthor = isValid(firstName, middleName, lastName);
-  
-    if (!validAuthor) return false;
-  
-    const [author] = await Author.createAuthor(firstName, middleName, lastName)
+  // Buscamos um autor com o mesmo nome completo que desejamos criar
+  const existingAuthor = await Author.findByName(firstName, middleName, lastName);
 
-  authorId = author.insertId;
+  // Caso essa pessoa autora já exista, retornamos um objeto de erro informando
+  // que não é possível criar a pessoa autora pois ele já existe
+  if (existingAuthor) {
+    return {
+      error: {
+        code: 'alreadyExists',
+        message: 'Uma pessoa autora já existe com esse nome completo',
+      },
+    };
+  }
 
-  return getNewAuthor({
-    id: authorId,
-    firstName,
-    middleName,
-    lastName,
-  })
-  };
+  // Caso a pessoa autora não exista e, portanto, possa ser criado
+  // chamamos o model e retornamos o resultado
+  return Author.createAuthor(firstName, middleName, lastName);
+};
+
+const findByName = async (firstName, middleName, lastName) => {
+  // Determinamos se devemos buscar com ou sem o nome do meio
+  let query = 'SELECT id, first_name, middle_name, last_name ' +
+              'FROM model_example.authors ';
+    if (middleName) {
+      query += 'WHERE first_name = ? AND middle_name = ? AND last_name = ?';
+    } else {
+      query += 'WHERE first_name = ? AND last_name = ?';
+    }
+
+  const params = middleName ? [firstName, middleName, lastName] : [firstName, lastName];
+
+  // Executamos a consulta e retornamos o resultado
+  const [authorData] = await connection.execute(query, params);
+
+  // Caso nenhum author seja encontrado, devolvemos null
+  if (authorData.length === 0) return null;
+
+  // Caso contrário, retornamos o author encontrado
+  return serialize(authorData);
+};
 
 module.exports = {
   getAll,
   findById,
-  createAuthor
-}
+  createAuthor,
+  findByName,
+};
